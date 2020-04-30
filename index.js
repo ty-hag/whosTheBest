12c9ae15faf37feb3b7defadff7486a11897b19d
tyhag-study <hagen.tyler.stuff@gmail.com>
2020-04-29 21:48:06 -0700

    Adds basic Discord connection, message read/write

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ Discord bot to keep track of who is the best
 
 */
 
+// db testing
+
 const db = require('./modules/database');
 
 const test = async () => {
@@ -16,3 +18,25 @@ const test = async () => {
 }
 
 test();
+
+// discord testing
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+const bestRegex = /(.*[!,.:;] )?(.*[^,])('s| is| iz|,? you're|,? you're|,? you are|,? you|,? u| are| r) (the|da|ze|duh|tha) (best|bez|bes|bestest)(.*[^\\?]$)/;
+
+client.on('message', message => {
+	if (bestRegex.test(message.content)) {
+    console.log(message.content);
+    const match = bestRegex.exec(message.content);
+    console.log(match);
+    message.channel.send(`${match[2]} has been declared the best!`);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
