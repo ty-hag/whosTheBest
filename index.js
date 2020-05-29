e91b1337dbc2f43acbd968f23d0dd4bde89be6bd
tyhag-study <hagen.tyler.stuff@gmail.com>
2020-05-29 16:57:03 -0700

    Working on tests and 2nd person declaration functionnality

--- index.js
+++ index.js
@@ -14,20 +14,22 @@ Testing
  - (NOT STARTED) Write test for new best declaration
 
 Handle pronouns
-- (DONE) 2nd person
-- (NOT STARTED) Buggy? If bot just booted and first input received is "you're the best", this line in handleDeclaration breaks bc empty string is passed for newBestWasAYou:
-    const newBest = newBestWasAYou ? newBestWasAYou : bestRegex.exec(message.content)[2];
+- (BUG) 2nd person
+ - (WIP) Is not 
 - (NOT STARTED) 3rd person
-- (NOT STARTED) Consider: if user says pronoun only, bot asks for specific name, if you again, bot is declared the best 
+  - (NOT STARTED) Consider: if user says pronoun only, bot asks for specific name, if you again, bot is declared the best 
 
 (DONE) Seed test database
+
+Send message to channel on join?
+https://stackoverflow.com/questions/45120618/send-a-message-with-discord-js
 
 */
 
 
 const handlers = require('./modules/handlers');
 const Discord = require('discord.js');
-const { youBestRegex, newBestRegex, checkBestRegex, rescindRegex } = require('./regexs');
+const { youBestRegex, ambiguousBestRegex, newBestRegex, checkBestRegex, rescindRegex } = require('./regexs');
 
 const client = new Discord.Client();
 
@@ -35,26 +37,43 @@ client.once('ready', () => {
   console.log('whosTheBest bot active!');
 });
 
-let authorOfLastMessage = '';
+// when user just says "you're the best" it should refer back to the last person to write a message
+// to the channel that is not the sender
+let messageAuthorTracker = {
+  lastActive: '',
+  activeAuthor: ''
+};
+
+let authorOfAmbiguousDeclaration = '';
 
 client.on('message', async message => {
+
+ if(message.author.username !== messageAuthorTracker.activeAuthor){
+  messageAuthorTracker.lastActive = messageAuthorTracker.activeAuthor;
+  messageAuthorTracker.activeAuthor = message.author.username;
+ }
 
   if (message.author.bot) {
     return;
   } else if (rescindRegex.test(message.content)) {
     await handlers.handleRescission(message);
   } else if (youBestRegex.test(message.content)) {
-    await handlers.handleDeclaration(message, authorOfLastMessage);
+    if(messageAuthorTracker.lastActive === '') {
+      message.channel.send(`Sorry, I've just joined the channel and I'm not sure who you're refering to.`);
+    } else {
+      await handlers.handleDeclaration(message, messageAuthorTracker.lastActive)
+    }
+  } else if (ambiguousBestRegex.test(message.content)) {
+    //await handlers.handleDeclaration(message, null);
+    console.log('ambiguous declaration detected!')
+    message.channel.send('ambiguous!');
   } else if (checkBestRegex.test(message.content)) {
     await handlers.handleQuestionCurrentBest(message);
   } else if (newBestRegex.test(message.content)) {
     await handlers.handleDeclaration(message, null);
   }
 
-  if (message.author.username !== authorOfLastMessage) {
-    authorOfLastMessage = message.author.username;
-  }
-
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
--- regexs.js
+++ regexs.js
@@ -1,6 +1,6 @@
 module.exports = {
-  youBestRegex: /^(.*, )?(you|you're|ur|you are) (the|da|tha) (best|bes|bestest)/i,
-  ambiguousBestRegex: /^(.*, )?(you|you're|ur|you are) (the|da|tha) (best|bes|bestest)/i,
+  youBestRegex: /(you|you're|ur|you are|u|u r) (the|da|tha) (best|bes|bestest)/i,
+  ambiguousBestRegex: /^(.*, )?(that's|that is|he is|he's|she is|she's|it is|it's|they are|they're|we're|we are|we r) (the|da|tha) (best|bes|bestest)/i,
   newBestRegex: /(.*[!,.:;] )?(.*[^,])('s| is| iz|,? you're|,? you're|,? you are|,? you|,? u| are| r) (the|teh|da|ze|duh|tha) (best|bez|bes|bestest)(.*[^\\?]$)/i,
   checkBestRegex: /(who's|who|who is) (the|da|ze) (best)[\\?]?/i,
   rescindRegex: /I take back that best declaration[.!]?/i,
